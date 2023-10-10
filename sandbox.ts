ca374b7c7a68cf820a733f75d28b2e085678045a
MikeSymoDev <mike04_norcal@yahoo.de>
2023-10-10 17:20:54 +0200

    basics, arrays, objects

--- sandbox.ts
+++ sandbox.ts
@@ -1,40 +1,63 @@
 // ----------------- ARRAYS ------------------------- //
+
 // let names = ['luigi', 'mario', 'yoshi'];
+
 // names.push('toad');
 // // names.push(3);
 // // names[0] = 3;
+
 // let numbers = [10, 20, 30, 40];
+
 // numbers.push[25];
 // // numbers[1] = 'shean'
 // // numbers.push('manny')
+
 // let mixed = ['ken', 4, 'barbie', 8, 9];
 // mixed.push('ryu');
 // mixed.push(3);
 // mixed[0] = 3;
+
+
 // ----------------- OBJECTS ------------------------- //
+
 // let ninja = {
 //   name: 'mario',
 //   belt: 'black',
 //   age: 30
 // };
+
 // ninja.age = 40;
 // ninja.name = 'ryu';
 // // ninja.age = '30';
 // // ninja.skills = ['fighting', 'sneaking']
+
 // ninja = {
 //   name: 'yoshi',
 //   belt: 'blue',
 //   age: 34,
 // }
+
+
+
+
 // ----------------- TYPE BASICS ------------------------- //
+
+
+
 // let character = 'mario';
 // let age = 30;
 // let isBlackBelt = false;
+
 // // character = 20
+
 // character = 'luigi';
+
 // // age = 'yoshi'
+
 // age = 40;
+
 // const circ = (diamater: number) => {
 //   return diamater * Math.PI
 // }
+
 // console.log(circ(7.5));
