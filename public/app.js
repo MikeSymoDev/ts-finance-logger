68e4f5aaa2eead3fd4deda1479fcd64c5cdaabb9
MikeSymoDev <mike04_norcal@yahoo.de>
2023-10-12 19:34:42 +0200

    modules and what they can do

--- public/app.js
+++ public/app.js
@@ -1,21 +1,4 @@
-"use strict";
-// classes
-class Invoice {
-    //   readonly client: string;
-    //   private details: string;
-    //   public amount: number;
-    //   constructor(c: string, d: string, a: number) {
-    //     (this.client = c), (this.details = d), (this.amount = a);
-    //   }
-    constructor(client, details, amount) {
-        this.client = client;
-        this.details = details;
-        this.amount = amount;
-    }
-    format() {
-        return `${this.client} owes $${this.amount} for ${this.details}`;
-    }
-}
+import { Invoice } from './classes/invoice.js';
 const invOne = new Invoice("mario", "work", 250);
 const invTwo = new Invoice("luigi", "bananas", 300);
 let invoices = [];
