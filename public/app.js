83b30f50cc643fd8915b20869c89bb28ee8bf50f
michaelsimonett <michael.simonett@youengineering.ch>
2023-10-13 10:43:14 +0200

    interfaces and classes

--- public/app.js
+++ public/app.js
@@ -1,12 +1,5 @@
-import { Invoice } from './classes/invoice.js';
-const invOne = new Invoice("mario", "work", 250);
-const invTwo = new Invoice("luigi", "bananas", 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, inv.amount, inv.format());
-});
+import { Invoice } from "./classes/invoice.js";
+import { Payment } from "./classes/payments.js";
 const form = document.querySelector(".new-item-form");
 // inputs
 const type = document.querySelector("#type");
@@ -15,7 +8,14 @@ const details = document.querySelector("#details");
 const amount = document.querySelector("#amount");
 form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
 // ----------------- DOM ------------------------- //
 // const anchor = document.querySelector('a')!;
@@ -24,3 +24,43 @@ form.addEventListener("submit", (e) => {
 // // }
 // console.log(anchor.href);
 // const form = document.querySelector('form')!;
+// ----------------- INTERFACES ------------------------- //
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+// const me: IsPerson = {
+//   name: 'mario',
+//   age: 30,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log('I spent', amount);
+//     return amount;
+//     },
+//   };
+// const greetPerson = (person: IsPerson) => {
+//   console.log('hello', person.name);
+// }
+// greetPerson(me);
+//   console.log(me);
+// ----------------- INTERFACES WITH CLASSES ------------------------- //
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+// docOne = new Invoice('yoshi', 'webwork', 250)
+// docTwo = new Payment('mario', 'pluming', 200);
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+// console.log(docs);
+// const invOne = new Invoice("mario", "work", 250);
+// const invTwo = new Invoice("luigi", "bananas", 300);
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,8 @@
-import { Invoice } from './classes/invoice.js'
+import { Invoice } from "./classes/invoice.js";
+import { Payment } from "./classes/payments.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
-const invOne = new Invoice("mario", "work", 250);
-const invTwo = new Invoice("luigi", "bananas", 300);
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-
-invoices.forEach((inv) => {
-  console.log(inv.client, inv.amount, inv.format());
-});
 
 const form = document.querySelector(".new-item-form") as HTMLFormElement;
 
@@ -23,7 +16,13 @@ const amount = document.querySelector("#amount") as HTMLInputElement;
 form.addEventListener("submit", (e: Event) => {
   e.preventDefault();
 
-  console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+let doc: HasFormatter;
+if(type.value === 'invoice') {
+  doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber)
+} else {
+  doc = new Payment(tofrom.value, details.value, amount.valueAsNumber)
+}
+  console.log(doc);
 });
 
 // ----------------- DOM ------------------------- //
@@ -37,3 +36,56 @@ form.addEventListener("submit", (e: Event) => {
 // console.log(anchor.href);
 
 // const form = document.querySelector('form')!;
+
+// ----------------- INTERFACES ------------------------- //
+
+// interface IsPerson {
+//   name: string;
+//   age: number;
+//   speak(a: string): void;
+//   spend(a: number): number;
+// }
+
+// const me: IsPerson = {
+//   name: 'mario',
+//   age: 30,
+//   speak(text: string): void {
+//     console.log(text);
+//   },
+//   spend(amount: number): number {
+//     console.log('I spent', amount);
+//     return amount;
+//     },
+//   };
+
+// const greetPerson = (person: IsPerson) => {
+//   console.log('hello', person.name);
+// }
+
+// greetPerson(me);
+//   console.log(me);
+
+// ----------------- INTERFACES WITH CLASSES ------------------------- //
+
+// let docOne: HasFormatter;
+// let docTwo: HasFormatter;
+
+// docOne = new Invoice('yoshi', 'webwork', 250)
+// docTwo = new Payment('mario', 'pluming', 200);
+
+// let docs: HasFormatter[] = [];
+// docs.push(docOne);
+// docs.push(docTwo);
+
+// console.log(docs);
+
+// const invOne = new Invoice("mario", "work", 250);
+// const invTwo = new Invoice("luigi", "bananas", 300);
+
+// let invoices: Invoice[] = [];
+// invoices.push(invOne);
+// invoices.push(invTwo);
+
+// invoices.forEach((inv) => {
+//   console.log(inv.client, inv.amount, inv.format());
+// });
